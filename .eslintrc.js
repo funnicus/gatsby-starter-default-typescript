45e2f81867c34f046447e9320a4b5e9a0ddf1520
funnicus <juhku@elisanet.fi>
2021-05-17 23:41:20 +0300

    initial version

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint',
+		'prettier'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			2
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'prettier/prettier': [
+			'error',
+			{
+			  'trailingComma': "es5",
+			  'semi': true,
+			  'singleQuote': true,
+			  'printWidth': 120,
+			},
+		],
+		'react/prop-types': 0
+	}
+};
